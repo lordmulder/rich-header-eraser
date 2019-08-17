e9985747dd75ccfd5158a57cefb30f3a1ad4d295
LoRd_MuldeR <mulder2@gmx.de>
2019-08-17 14:08:25 +0200

    Added option '--zero' to overwrite with NULL bytes instead of "random" ones.

--- src/rchhdrrsr.cpp
+++ src/rchhdrrsr.cpp
@@ -12,7 +12,16 @@
 #include "rand.h"
 #include "utils.h"
 
+static const unsigned VERSION_MAJOR = 1U;
+static const unsigned VERSION_MINOR = 0U;
+static const unsigned VERSION_PATCH = 1U;
+
 static const DWORD DOS_STUB_LEN = 0x80;
+
+#define CLEANUP_RETURN(X) \
+	destroy_mapping(&mapping); \
+	CloseHandle(file); \
+	return (X);
 
 static BOOL check_hdr_mz(const BYTE *const data, const DWORD size)
 {
@@ -42,7 +51,7 @@ static BOOL locate_rich_footer(const BYTE *const data, const DWORD pe_offset, DW
 static BOOL locate_rich_header(const BYTE *const data, const DWORD foor_offset, DWORD *const header_offset_out)
 {
 	BOOL found = false;
-	for (DWORD off = DOS_STUB_LEN; off < foor_offset - 3U; ++off)
+	for (DWORD off = foor_offset - 4U; off >= DOS_STUB_LEN; --off)
 	{
 		if (((data[off + 0U] ^ data[foor_offset + 4U]) == 'D') && ((data[off + 1U] ^ data[foor_offset + 5U]) == 'a') &&
 			((data[off + 2U] ^ data[foor_offset + 6U]) == 'n') && ((data[off + 3U] ^ data[foor_offset + 7U]) == 'S'))
@@ -57,14 +66,43 @@ static BOOL locate_rich_header(const BYTE *const data, const DWORD foor_offset, 
 
 static int rchhdrrsr(const int argc, const WCHAR *const *const argv)
 {
-	printf(L"Rich-Header Eraser [%S]\n\n", __DATE__);
+	printf(L"Rich-Header Eraser v%u.%02u-%u [%S]\n\n", VERSION_MAJOR, VERSION_MINOR, VERSION_PATCH, __DATE__);
 
 	if (argc < 2)
 	{
-		puts(L"Usage:\n  rchhdrrsr.exe <path_to_binary>\n\n");
+		puts(L"Usage:\n  rchhdrrsr.exe [--zero] <path_to_binary>\n\n");
 		return 1;
 	}
-	else if (argc > 2)
+
+	int arg_idx = 1;
+	BOOL zero = FALSE;
+
+	for(; arg_idx < argc; ++arg_idx)
+	{
+		if((argv[arg_idx][0U] == L'-') && (argv[arg_idx][1U] == L'-'))
+		{
+			if(argv[arg_idx][2U] == L'\0')
+			{
+				++arg_idx;
+				break;
+			}
+			if(!lstrcmpiW(argv[arg_idx], L"--zero"))
+			{
+				zero = TRUE;
+				continue;
+			}
+			printf(L"Error: Option \"%s\" is unknown!\n\n", argv[arg_idx]);
+			return 2;
+		}
+		break;
+	}
+
+	if ((arg_idx >= argc) || (!argv[arg_idx][0U]))
+	{
+		puts(L"Error: Target file name has *not* been specified!\n\n");
+		return 3;
+	}
+	else if (arg_idx < argc - 1)
 	{
 		puts(L"Warning: Excess command-line argument was ignored!\n\n");
 	}
@@ -72,24 +110,24 @@ static int rchhdrrsr(const int argc, const WCHAR *const *const argv)
 	puts(L"Mapping binary file into memory... ");
 
 	DWORD error = 0U;
-	const HANDLE file = open_file(argv[1], GENERIC_READ | GENERIC_WRITE, OPEN_EXISTING, &error);
+	const HANDLE file = open_file(argv[arg_idx], GENERIC_READ | GENERIC_WRITE, OPEN_EXISTING, &error);
 	if (file == INVALID_HANDLE_VALUE)
 	{
-		printf(L"Failed!\n\nSpecified file could *not* be opened for reading/writing:\n%s\n\n", argv[1]);
+		printf(L"Failed!\n\nSpecified file could *not* be opened for reading/writing:\n%s\n\n", argv[arg_idx]);
 		if(WCHAR *const messsage = get_error_message(error))
 		{
 			printf(L"%s\n\n", messsage);
 			LocalFree((HLOCAL)messsage);
 		}
-		return 2;
+		return 4;
 	}
 
 	mapping_t mapping;
 	if(!create_mapping(file, &mapping, 4096U))
 	{
-		CloseHandle(file);
 		puts(L"Failed!\n\nError: Failed to map file into memory!\n\n");
-		return 3;
+		CloseHandle(file);
+		return 5;
 	}
 
 	puts(L"OK\nScanning for PE header... ");
@@ -97,18 +135,14 @@ static int rchhdrrsr(const int argc, const WCHAR *const *const argv)
 	if (!check_hdr_mz(mapping.view, mapping.size))
 	{
 		puts(L"Failed!\n\nFile does *not* look like a valid EXE or DLL file!\n\n");
-		destroy_mapping(&mapping);
-		CloseHandle(file);
-		return 4;
+		CLEANUP_RETURN(6)
 	}
 
 	DWORD pe_offset = 0U;
 	if (!check_hdr_pe(mapping.view, mapping.size, &pe_offset))
 	{
 		puts(L"Failed!\n\nFile does *not* contain a proper PE format header!\n\n");
-		destroy_mapping(&mapping);
-		CloseHandle(file);
-		return 5;
+		CLEANUP_RETURN(7)
 	}
 
 	puts(L"OK\nScanning for Rich (DanS) header... ");
@@ -117,32 +151,32 @@ static int rchhdrrsr(const int argc, const WCHAR *const *const argv)
 	if (!locate_rich_footer(mapping.view, pe_offset, &rich_footer))
 	{
 		puts(L"Failed!\n\nFile does *not* seem to contain Rich (DanS) header. Already erased or not created by M$?\n\n");
-		destroy_mapping(&mapping);
-		CloseHandle(file);
-		return 6;
+		CLEANUP_RETURN(8)
 	}
 
 	DWORD rich_header = 0U;
 	if (!locate_rich_header(mapping.view, rich_footer, &rich_header))
 	{
 		puts(L"Failed!\n\nFound a Rich header, but 'DanS' signature could *not* be decoded!\n\n");
-		destroy_mapping(&mapping);
-		CloseHandle(file);
-		return 7;
+		CLEANUP_RETURN(9)
 	}
 
 	printf(L"OK\nErasing 0x%04X to 0x%04X... ", rich_header, rich_footer + 7U);
 
 	for (DWORD off = rich_header; off < rich_footer + 8U; ++off)
 	{
-		mapping.view[off] = rnd_next();
+		mapping.view[off] = zero ? 0x00 : rnd_next_byte();
 	}
 
 	FlushViewOfFile(mapping.view, mapping.size);
-	puts(L"OK\n\nCompleted.\n");
-
 	destroy_mapping(&mapping);
+
+	puts(L"OK\n\n");
+
+	FlushFileBuffers(file);
 	CloseHandle(file);
+
+	puts(L"Completed.\n\n");
 	return 0;
 }
 
@@ -152,7 +186,6 @@ int wmain(const int argc, const WCHAR *const *const argv)
 	con_init();
 	rnd_init();
 	retval = rchhdrrsr(argc, argv);
-	rnd_exit();
 	con_exit();
 	return retval;
 }
