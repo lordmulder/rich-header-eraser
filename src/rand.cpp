ce055da51eb95535d34253659512f77b9c999bae
LoRd_MuldeR <mulder2@gmx.de>
2019-09-08 16:12:54 +0200

    Revamped rnd_update() function.

--- src/rand.cpp
+++ src/rand.cpp
@@ -10,14 +10,14 @@
 #include "rand.h"
 #include <limits.h>
 
-#define RESEED_COUNT 2147483647U
+#define RESEED_COUNT 999983U
 #define MIX(X,Y,Z) \
 	a += (X); b += (Y); c += (Z); \
 	mix_function(a, b, c);
 
 static CRITICAL_SECTION g_mutex;
-static DWORD g_state[4U] = { 0x4F5B7CF1, 0x599531DE, 0xBC360195, 0x2933231A };
-static DWORD g_reseed_counter = RESEED_COUNT;
+static DWORD g_state[6U] = { 0x4F5B7CF1, 0x599531DE, 0xBC360195, 0x2933231A, 0x62DFEC75, 0x959E2017 };
+static SIZE_T g_reseed_counter = RESEED_COUNT;
 static DWORD g_byte_buffer = MAXDWORD;
 static SIZE_T g_byte_counter = sizeof(DWORD);
 
@@ -32,6 +32,11 @@ static void mix_function(DWORD &a, DWORD &b, DWORD &c)
 	a = a - b; a = a - c; a = a ^ (c >>  3);
 	b = b - c; b = b - a; b = b ^ (a << 10);
 	c = c - a; c = c - b; c = c ^ (b >> 15);
+}
+
+static DWORD combine(const DWORD a, const DWORD b)
+{
+	return a ^ (b + 0x9E3779B9 + (a << 6U) + (a >> 2U));
 }
 
 static DWORD get_entropy(const DWORD seed)
@@ -51,20 +56,23 @@ static DWORD get_entropy(const DWORD seed)
 
 static void rnd_seed(void)
 {
-	for (UINT i = 0U; i < 13U; i++)
+	for (UINT i = 0U; i < 13U; ++i)
 	{
 		g_state[0U] = get_entropy(g_state[0U]);
-		g_state[1U] = get_entropy(g_state[1U]);
-		g_state[2U] = get_entropy(g_state[2U]);
 		g_state[3U] = get_entropy(g_state[3U]);
+		g_state[1U] = get_entropy(g_state[1U]);
+		g_state[4U] = get_entropy(g_state[4U]);
+		g_state[2U] = get_entropy(g_state[2U]);
+		g_state[5U] = get_entropy(g_state[5U]);
 	}
 }
 
 static void rnd_update(void)
 {
-	g_state[2U] += 1U; mix_function(g_state[0U], g_state[1U], g_state[2U]);
-	g_state[1U] += 1U; mix_function(g_state[2U], g_state[0U], g_state[1U]);
-	g_state[0U] += 1U; mix_function(g_state[1U], g_state[2U], g_state[0U]);
+	g_state[0U] += 1U;
+	mix_function(g_state[2U], g_state[1U], g_state[0U]);
+	g_state[3U] -= 1U;
+	mix_function(g_state[5U], g_state[4U], g_state[3U]);
 }
 
 static DWORD _rnd_next(void)
@@ -75,7 +83,7 @@ static DWORD _rnd_next(void)
 		g_reseed_counter = 0U;
 	}
 	rnd_update();
-	return g_state[0U] ^ g_state[3U];
+	return combine(g_state[0U], g_state[3U]);
 }
 
 static BYTE _rnd_byte(void)
